998cc80fee2513af72cc72f474418dedae3d431a
zhanghangwade3 <zhanghangwade@126.com>
2018-02-02 00:33:58 +0800

    Initial commit

---
+++ app/src/main/java/androidhive/info/materialdesign/activity/FriendsFragment.java
@@ -0,0 +1,31 @@
+package androidhive.info.materialdesign.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidhive.info.materialdesign.R;
+
+
+public class FriendsFragment extends Fragment {
+
+
+    public FriendsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_friends, container, false);
+    }
+}
---
+++ app/src/main/java/androidhive/info/materialdesign/activity/MessagesFragment.java
@@ -0,0 +1,31 @@
+package androidhive.info.materialdesign.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidhive.info.materialdesign.R;
+
+
+public class MessagesFragment extends Fragment {
+
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_messages, container, false);
+    }
+}
